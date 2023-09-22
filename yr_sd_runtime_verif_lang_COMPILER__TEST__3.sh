896c88d7e7a6c503493b570eda2f292206d00941
PROF. Dr.-Ing. Dipl.-Inf. Xavier Noumbissi Noundou <yeroth.d@gmail.com>
2023-09-22 19:54:31 -0400

    UPDATED. i have actualized sample runtime monitor generation scripts.

--- yr_sd_runtime_verif_lang_COMPILER__TEST__3.sh
+++ yr_sd_runtime_verif_lang_COMPILER__TEST__3.sh
@@ -2,35 +2,7 @@
 # @AUTEUR: DR.-ING. DIPL.-INF. XAVIER NOUMBISSI NOUNDOU
 #
 
-# SHOWS THIS Makefile FILE COMMAND STRING IN THE
-# CONSOLE AS THEY ARE EXECUTED.
 set -x
 
-yr_sd_runtime_verif_lang_comp < test_inputs/yr_test_input_3.spec_sd_mealy
-
-
-# Replaces file name with prefix 'yr-db-runtime-verif-main-GENERATED'
-# with new file prefix 'yr-db-runtime-verif-main'.
-
-GENERATED_FILE_PREFIX="yr-db-runtime-verif-main-GENERATED"
-
-NEW_FILE_FROM_GENERATED_FILE_PREFIX="yr-db-runtime-verif-main"
-
-
-yerothx-rename-files.sh -o "${GENERATED_FILE_PREFIX}" \
-	-n "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}" * 
-
-
-# Replacing all occurences of string 'yr-db-runtime-verif-main-GENERATED'
-# by string 'yr-db-runtime-verif-main'.
-
-yerothx-replace-string.sh -o "${GENERATED_FILE_PREFIX}" \
-	-n "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}" "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}".hpp
-
-yerothx-replace-string.sh -o "${GENERATED_FILE_PREFIX}" \
-	-n "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}" "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}".cpp
-
-astyle "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}".hpp 
-
-astyle "${NEW_FILE_FROM_GENERATED_FILE_PREFIX}".cpp
+./yr_sd_runtime_verif_lang_COMPILER__generation.sh -i test_inputs/yr_test_input_3.spec_sd_mealy
 
